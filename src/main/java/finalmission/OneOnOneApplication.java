9aab624847d9f5539a1e322769b2c9510cbdd0bd
sonjh919 <kingjh1125@gmail.com>
2025-09-03 13:26:42 +0900

    init dev

---
+++ src/main/java/finalmission/OneOnOneApplication.java
@@ -0,0 +1,13 @@
+package finalmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneOnOneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OneOnOneApplication.class, args);
+  }
+
+}
